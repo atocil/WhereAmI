121aa92ddb8cac92d532a51d93377e1ddbdf26a9
Joseph Klonowski <klonowskijoseph@gmail.com>
2014-02-01 12:34:10 -0600

    I fix colors

--- colors.js
+++ colors.js
@@ -1,18 +1,5 @@
 var colors = new Array();
 
-<<<<<<< HEAD
-colors[0] = 0x0000ff;
-colors[1] = 0xff0000;
-colors[2] = 0x00ff00;
-colors[3] = 0x4682B4;
-colors[4] = 0xffff00;
-colors[5] = 0x00ffff;
-colors[6] = 0xC0C0C0;
-colors[7] = 0xFF7F50;
-colors[8] = 0xffA500;
-colors[9] = 0x800080;
-colors[10] = 0x7fff00;
-=======
 colors[0] = 0x0000ff; //BLUE
 colors[1] = 0xff0000; //RED
 colors[2] = 0x00ff00; //GREEN
@@ -24,7 +11,5 @@ colors[7] = 0x4F2F4F; //VIOLET
 //colors[8] = 0x
 //colors[9] = 0x
 //colors[10] = 0x
->>>>>>> 0d4327d6553b04f77bda76acf593e7df5a025a17
-
 colors[11] = 0x000000;
 colors[12] = 0xffffff;
