363b60c523a71be7f8254db8ee862a6e24b4fa9d
Joseph Klonowski <klonowskijoseph@gmail.com>
2014-01-31 21:08:26 -0600

    World Generation
    I hope it works.

---
+++ worldgen.js
@@ -0,0 +1,52 @@
+function generate_world(num_rooms, num_doors)
+{
+	var rooms = new Array();
+
+	for(var i = 0; i < num_rooms; i ++) {
+		rooms[i] = new Room();
+
+	}
+
+	var temp_room = room[0];
+
+	for(i = 0; i < num_doors; i ++) {
+
+		for(var j = 0; j < num_rooms; j ++) {
+			temp_room.visited = true;
+
+			//Which door this path will leave from
+			var door_num = int(Math.random() * num_doors);
+			while(temp_room.paths[door_num] == null) {
+				door_num = int(Math.random() * num_doors);
+			}
+
+			//Which room this path will go to
+			var to_room = int(Math.random() * num_rooms);
+			while(rooms[to_room].visited) {
+				to_room = int(Math.random() * num_rooms);
+			}
+
+			//Which door you will emerge from in your new room
+			var to_door = int(Math.random() * num_doors);
+
+			temp_room.paths[door_num] = new Path(to_room, to_door);
+
+			temp_room = rooms[to_room];
+		}
+
+		while(temp_room.paths[door_num] == null) {
+				door_num = int(Math.random() * num_doors);
+		}
+
+		temp_room.paths[door_num] = new Path(int(Math.random() * num_rooms), int(Math.random() * num_doors));
+
+		reset_rooms_visited(rooms);
+	}
+}
+
+function reset_rooms_visited(rooms)
+{
+	for(var i = 0; i < rooms.length; i ++) {
+		rooms[i].visited = false;
+	}
+}
